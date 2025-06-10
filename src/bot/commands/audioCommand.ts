5475d01660361d4250c9d974d723db9ef5ef1280
Giuseppe Cutuli <cutuligiuseppe95@gmail.com>
2025-06-10 21:48:41 +0200

    fix: change API for send audio

--- src/bot/commands/audioCommand.ts
+++ src/bot/commands/audioCommand.ts
@@ -4,7 +4,7 @@ import { getRandomAudioFile } from '../../utils/audioManager';
 export const audioCommand = async (ctx: Context) => {
     const audioFile = getRandomAudioFile();
     if (audioFile) {
-        await ctx.replyWithAudio({ source: audioFile.file });
+        await ctx.replyWithVoice({ source: audioFile.file });
     } else {
         await ctx.reply('No audio files available.');
     }
