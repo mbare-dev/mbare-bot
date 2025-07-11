25f4701771a6267ba0b0f2cb8d96c7d7e1f0d3a0
riccardobattiato <riccardobattiato@proton.me>
2025-07-11 17:35:34 +0200

    feat: help for new a virita command

--- src/bot/commands/help/help.ts
+++ src/bot/commands/help/help.ts
@@ -9,12 +9,13 @@ export const help = {
             { command: 'quote', description: 'Scegli una citazione da riprodurre' },
             { command: 'randomQuote', description: 'Invia una citazione casuale' },
             { command: 'help', description: 'Mostra questo messaggio di aiuto' },
+            { command: 'a-virita', description: 'Invia un "a virita" casuale' },
             // Aggiungi qui altri comandi disponibili
         ];
 
         const helpMessage = `🤖 *MBARE BOT \\- Comandi Disponibili*\n\n${
             commands.map(cmd => `• /${cmd.command} \\- ${cmd.description}`).join('\n')
-        }\n\nPer ulteriori informazioni visita il nostro sito: https://mbare\\.dev`;
+        }\n\nPer ulteriori informazioni visita il nostro sito: https://mbare\\.dev (sucuni)`;
 
         await ctx.replyWithMarkdownV2(helpMessage);
     },
